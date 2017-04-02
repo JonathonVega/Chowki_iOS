5a209ccb974dbde7d13030bf8f714f74941f31db
Jonathon Vega <jfvega@live.com>
2017-04-01 23:53:26 -0400

    Initial Commit

---
+++ Chowki/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Chowki
+//
+//  Created by Jonathon F Vega on 4/1/17.
+//  Copyright © 2017 Jonathon Vega. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
